c8940dd61900e4f0063602686d9f7988d50130f6
Guilherme-Zatta <guilhermezatta@hotmail.com>
2024-05-03 20:02:55 -0300

    Primeiro commit

---
+++ src/Exercicio02.java
@@ -0,0 +1,6 @@
+public class Exercicio02 {
+    public static void main(String[] args) {
+        //Exercício 02 Gerar e escrever uma matriz bidimensional de tamanho 9x7, com números aleatórios entre zero e cem. Ao final, imprimir a média geral dos números da tabela.
+        
+    }
+}
